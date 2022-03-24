980fe0c6de66c60212f7ad137848538b3417a65a
Jason Hillman <j.hillman@berkeley.edu>
2022-03-23 23:27:06 -0700

    day 4 part 2 complete (python)

--- day4/solve.py
+++ day4/solve.py
@@ -16,6 +16,11 @@ def check(name):
     msg = ''.join(map(lambda x: x[0], srt))[:5]
     return msg
 
+def decrypt(cipher, shift):
+    ltrs = [ chr(((ord(letter) - 97 + shift) % 26) + 97) for letter in cipher ]
+    msg = ''.join(ltrs)
+    return msg
+
 
 with open('day4/input.txt') as f:
     lines = f.read().splitlines()
@@ -24,3 +29,11 @@ with open('day4/input.txt') as f:
 idsum = sum([ id for name, id, cksum in map(parse, lines) if check(name) == cksum ])
 
 print(idsum)
+
+for line in lines:
+    name, id, _ = parse(line)
+    msg = decrypt(name, id)
+    if 'northpole' in msg:
+        sector = id
+
+print(sector)
